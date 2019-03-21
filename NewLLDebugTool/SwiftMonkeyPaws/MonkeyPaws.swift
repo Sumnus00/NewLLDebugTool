21b6c234631c9f3c8fc11ed1ba4ee7a41f2a75a0
haleli <haleli@tencent.com>
2019-03-21 15:43:11 +0800

    提交 podspec

--- NewLLDebugTool/SwiftMonkeyPaws/MonkeyPaws.swift
+++ NewLLDebugTool/SwiftMonkeyPaws/MonkeyPaws.swift
@@ -38,15 +38,15 @@ private let circleRadius: CGFloat = 7
 public class MonkeyPaws: NSObject, CALayerDelegate {
     private var gestures: [(hash: Int?, gesture: Gesture)] = []
     private weak var view: UIView?
-    
+
     let layer = CALayer()
-    
+
     fileprivate static var tappingTracks: [WeakReference<MonkeyPaws>] = []
-    
+
     /**
      Create a MonkeyPaws object that will visualise input
      events.
-     
+
      - parameter view: The view to put the visualisation
      layer in. Usually, you will want to pass your main
      `UIWindow` here.
@@ -59,20 +59,20 @@ public class MonkeyPaws: NSObject, CALayerDelegate {
     @objc public init(view: UIView, tapUIApplication: Bool = true) {
         super.init()
         self.view = view
-        
+
         layer.delegate = self
         layer.isOpaque = false
         layer.frame = view.layer.bounds
         layer.contentsScale = UIScreen.main.scale
         layer.rasterizationScale = UIScreen.main.scale
-        
+
         view.layer.addSublayer(layer)
-        
+
         if tapUIApplication {
             tapUIApplicationSendEvent()
         }
     }
-    
+
     /**
      If you have disabled UIApplication event tapping,
      use this method to pass in `UIEvent` objects to
@@ -81,27 +81,27 @@ public class MonkeyPaws: NSObject, CALayerDelegate {
     public func append(event: UIEvent) {
         guard event.type == .touches else { return }
         guard let touches = event.allTouches else { return }
-        
+
         for touch in touches {
             append(touch: touch)
         }
-        
+
         bumpAndDisplayLayer()
     }
-    
+
     func append(touch: UITouch) {
         guard let view = view else { return }
-        
+
         let touchHash = touch.hash
         let point = touch.location(in: view)
-        
+
         let index = gestures.index(where: { (gestureHash, _) -> Bool in
             return gestureHash == touchHash
         })
-        
+
         if let index = index {
             let gesture = gestures[index].gesture
-            
+
             if touch.phase == .ended {
                 gestures[index].gesture.end(at: point)
                 gestures[index].hash = nil
@@ -115,50 +115,50 @@ public class MonkeyPaws: NSObject, CALayerDelegate {
             if gestures.count > maxGesturesShown {
                 gestures.removeFirst()
             }
-            
+
             gestures.append((hash: touchHash, gesture: Gesture(from: point, inLayer: layer)))
-            
+
             for i in 0 ..< gestures.count {
                 gestures[i].gesture.number = gestures.count - i
             }
         }
     }
-    
+
     private static let swizzleMethods: Bool = {
         let originalSelector = #selector(UIApplication.sendEvent(_:))
         let swizzledSelector = #selector(UIApplication.monkey_sendEvent(_:))
-        
+
         let originalMethod = class_getInstanceMethod(UIApplication.self, originalSelector)
         let swizzledMethod = class_getInstanceMethod(UIApplication.self, swizzledSelector)
-        
+
         let didAddMethod = class_addMethod(UIApplication.self, originalSelector, method_getImplementation(swizzledMethod!), method_getTypeEncoding(swizzledMethod!))
-        
+
         if didAddMethod {
             class_replaceMethod(UIApplication.self, swizzledSelector, method_getImplementation(originalMethod!), method_getTypeEncoding(originalMethod!))
         } else {
             method_exchangeImplementations(originalMethod!, swizzledMethod!)
         }
-        
+
         return true
     }()
-    
+
     private func tapUIApplicationSendEvent() {
         _ = MonkeyPaws.swizzleMethods
         MonkeyPaws.tappingTracks.append(WeakReference(self))
     }
-    
+
     private func bumpAndDisplayLayer() {
         guard let superlayer = layer.superlayer else { return }
         guard let layers = superlayer.sublayers else { return }
         guard let index = layers.index(of: layer) else { return }
-        
+
         if index != layers.count - 1 {
             layer.removeFromSuperlayer()
             superlayer.addSublayer(layer)
         }
-        
+
         layer.frame = superlayer.bounds
-        
+
         layer.setNeedsDisplay()
         layer.displayIfNeeded()
     }
@@ -166,21 +166,21 @@ public class MonkeyPaws: NSObject, CALayerDelegate {
 
 private class Gesture {
     var points: [CGPoint]
-    
+
     var containerLayer = CALayer()
     var startLayer = CAShapeLayer()
     var numberLayer = CATextLayer()
     var pathLayer: CAShapeLayer?
     var endLayer: CAShapeLayer?
-    
+
     private static var counter: Int = 0
-    
+
     init(from: CGPoint, inLayer: CALayer) {
         self.points = [from]
-        
+
         let counter = Gesture.counter
         Gesture.counter += 1
-        
+
         let angle = 45 * (CGFloat(fmod(Float(counter) * 0.279, 1)) * 2 - 1)
         let mirrored = counter % 2 == 0
         let colour = UIColor(hue: CGFloat(fmod(Float(counter) * 0.391, 1)), saturation: 1, brightness: 0.5, alpha: 1)
@@ -189,7 +189,7 @@ private class Gesture {
         startLayer.fillColor = nil
         startLayer.position = from
         containerLayer.addSublayer(startLayer)
-        
+
         numberLayer.string = "1"
         numberLayer.bounds = CGRect(x:0, y: 0, width: 32, height: 13)
         numberLayer.fontSize = 10
@@ -198,100 +198,100 @@ private class Gesture {
         numberLayer.position = from
         numberLayer.contentsScale = UIScreen.main.scale
         containerLayer.addSublayer(numberLayer)
-        
+
         inLayer.addSublayer(containerLayer)
     }
-    
+
     deinit {
         containerLayer.removeFromSuperlayer()
     }
-    
-    
+
+
     var number: Int = 0 {
         didSet {
             numberLayer.string = String(number)
-            
+
             let fraction = Float(number - 1) / Float(maxGesturesShown)
             let alpha = sqrt(1 - fraction)
             containerLayer.opacity = alpha
         }
     }
-    
+
     func extend(to: CGPoint) {
         guard let startPath = startLayer.path,
             let startPoint = points.first else {
                 assertionFailure("No start marker layer exists")
                 return
         }
-        
+
         points.append(to)
-        
+
         let pathLayer = self.pathLayer ?? { () -> CAShapeLayer in
             let newLayer = CAShapeLayer()
             newLayer.strokeColor = startLayer.strokeColor
             newLayer.fillColor = nil
-            
+
             let maskPath = CGMutablePath()
             maskPath.addRect(CGRect(x: -10000, y: -10000, width: 20000, height: 20000))
             maskPath.addPath(startPath)
-            
+
             let maskLayer = CAShapeLayer()
             maskLayer.path = maskPath
             maskLayer.fillRule = CAShapeLayerFillRule.evenOdd
             maskLayer.position = startLayer.position
             newLayer.mask = maskLayer
-            
+
             self.pathLayer = newLayer
             containerLayer.addSublayer(newLayer)
-            
+
             return newLayer
             }()
-        
+
         let path = CGMutablePath()
         path.move(to: startPoint)
         for point in points.dropFirst() {
             path.addLine(to: point)
         }
-        
+
         pathLayer.path = path
     }
-    
+
     func end(at: CGPoint) {
         guard endLayer == nil else {
             assertionFailure("Attempted to end or cancel a gesture twice!")
             return
         }
-        
+
         extend(to: at)
-        
+
         let layer = CAShapeLayer()
         layer.strokeColor = startLayer.strokeColor
         layer.fillColor = nil
         layer.position = at
-        
+
         let path = circlePath()
         layer.path = path.cgPath
-        
+
         containerLayer.addSublayer(layer)
         endLayer = layer
     }
-    
+
     func cancel(at: CGPoint) {
         guard endLayer == nil else {
             assertionFailure("Attempted to end or cancel a gesture twice!")
             return
         }
-        
+
         extend(to: at)
-        
+
         let layer = CAShapeLayer()
         layer.strokeColor = startLayer.strokeColor
         layer.fillColor = nil
         layer.position = at
-        
+
         let path = crossPath()
         layer.path = path.cgPath
-        
+
         containerLayer.addSublayer(layer)
         endLayer = layer
     }
@@ -319,17 +319,17 @@ private func monkeyHandPath(angle: CGFloat, scale: CGFloat, mirrored: Bool) -> U
     bezierPath.addCurve(to: CGPoint(x: 8.3, y: 7.11), controlPoint1: CGPoint(x: 7.76, y: 1.6), controlPoint2: CGPoint(x: 9.4, y: 4.35))
     bezierPath.addCurve(to: CGPoint(x: -5.91, y: 8.76), controlPoint1: CGPoint(x: 7.21, y: 9.86), controlPoint2: CGPoint(x: -2.63, y: 10.41))
     bezierPath.close()
-    
+
     bezierPath.apply(CGAffineTransform(translationX: 0.5, y: 0))
-    
+
     bezierPath.apply(CGAffineTransform(scaleX: scale, y: scale))
-    
+
     if mirrored {
         bezierPath.apply(CGAffineTransform(scaleX: -1, y: 1))
     }
-    
+
     bezierPath.apply(CGAffineTransform(rotationAngle: angle / 180 * CGFloat.pi))
-    
+
     return bezierPath
 }
 
@@ -359,14 +359,14 @@ extension UIApplication {
                 track.append(event: event)
             }
         }
-        
+
         self.monkey_sendEvent(event)
     }
 }
 
 extension CGRect {
     public init(centre: CGPoint, size: CGSize) {
-        self.origin = CGPoint(x: centre.x - size.width / 2, y: centre.y - size.height / 2)
-        self.size = size
+        self.init(origin: CGPoint(x: centre.x - size.width / 2, y: centre.y - size.height / 2),
+                  size: size)
     }
 }
