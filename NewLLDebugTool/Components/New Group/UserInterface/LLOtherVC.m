b85a610c0477e354c74e7461bd6b03ef10fc14cf
haleli <haleli@tencent.com>
2019-05-13 20:21:41 +0800

    cocos creator monkey 支持 ios 混合

--- NewLLDebugTool/Components/New Group/UserInterface/LLOtherVC.m
+++ NewLLDebugTool/Components/New Group/UserInterface/LLOtherVC.m
@@ -367,68 +367,77 @@ static NSString *const kLLOtherVCHeaderID = @"LLOtherHeaderID";
 }
 
 - (void)randomCocosMonkey{
-    int width = self.view.bounds.size.width ;
-    int height = self.view.bounds.size.height ;
-    int x = arc4random() % width  ;
-    int y = arc4random() % height ;
-    int seed = arc4random() % 10 ;
-    
-    //10%的概率发送滑动事件
-    if(seed<1){
-        int endX = arc4random() % width ;
-        int endY = arc4random() % height ;
-        NSLog(@"haleli >>>> test monkey,swip(%d,%d) to (%d,%d)",x,y,endX,endY) ;
-        [self swapWithPoint:CGPointMake(x, y) endPoint:CGPointMake(endX, endY)] ;
-      //10%的概率发送click事件
-    }else if(seed<2){
-        NSLog(@"haleli >>>> test monkey,click(%d,%d)",x,y) ;
-        [self touchesWithPoint:CGPointMake(x,y)];
-    }else if(seed < 10){
-        if([LLDebugTool sharedTool].ccTree){
-            
-            double width = MAX([[UIScreen mainScreen] bounds].size.width,[[UIScreen mainScreen] bounds].size.height) ;
-            double height = MIN([[UIScreen mainScreen] bounds].size.width,[[UIScreen mainScreen] bounds].size.height) ;
-            
-            NSDictionary *tree = [LLDebugTool sharedTool].ccTree() ;
+    UIViewController *controller = [FindTopController topController] ;
+    //cocos 页面
+    if([controller isKindOfClass:[[UIApplication sharedApplication].keyWindow.rootViewController class]]){
+        NSLog(@"haleli >>>> 页面 属于 cocos") ;
+        int width = self.view.bounds.size.width ;
+        int height = self.view.bounds.size.height ;
+        int x = arc4random() % width  ;
+        int y = arc4random() % height ;
+        int seed = arc4random() % 10 ;
         
-            NSArray *children = [tree objectForKey:@"children"] ;
-            
-            NSDictionary *child = nil ;
-             //10%的返回事件
-            if(seed < 3){
-                if([children count] > 0){
-                    
-                    child = [children lastObject] ;
-                    NSLog(@"haleli >>>> test monkey,back action , click name : %@ ",[child objectForKey:@"name"]) ;
-                    if(![[[child objectForKey:@"name"] lowercaseString] containsString:@"back"]){
-                        [self touchesWithPoint:CGPointMake(344,32)];
-                        return ;
-                    }
-                   
-                }
-            //70%的概率发送UI事件
-            }else{
-                if([children count] > 1){
-                    int random = arc4random() % ([children count] - 1);
-                    child = [children objectAtIndex:random] ;
-                    
-                    NSLog(@"haleli >>>> test monkey,ui action , click name : %@ ",[child objectForKey:@"name"]) ;
-                }
-            }
-            
-            NSDictionary *payload = [child objectForKey:@"payload"] ;
-
-            NSArray *pos = [payload objectForKey:@"pos"] ;
-            
-            
-            double x = [[pos objectAtIndex:0] doubleValue] * width;
-            double y = [[pos objectAtIndex:1] doubleValue] * height;
+        //10%的概率发送滑动事件
+        if(seed<1){
+            int endX = arc4random() % width ;
+            int endY = arc4random() % height ;
+            NSLog(@"haleli >>>> test monkey,swip(%d,%d) to (%d,%d)",x,y,endX,endY) ;
+            [self swapWithPoint:CGPointMake(x, y) endPoint:CGPointMake(endX, endY)] ;
+            //10%的概率发送click事件
+        }else if(seed<2){
+            NSLog(@"haleli >>>> test monkey,click(%d,%d)",x,y) ;
             [self touchesWithPoint:CGPointMake(x,y)];
-            
-            
-        }else{
-            NSLog(@"cocos creator don't get tree") ;
+        }else if(seed < 10){
+            if([LLDebugTool sharedTool].ccTree){
+                
+                double width = MAX([[UIScreen mainScreen] bounds].size.width,[[UIScreen mainScreen] bounds].size.height) ;
+                double height = MIN([[UIScreen mainScreen] bounds].size.width,[[UIScreen mainScreen] bounds].size.height) ;
+                
+                NSDictionary *tree = [LLDebugTool sharedTool].ccTree() ;
+                
+                NSArray *children = [tree objectForKey:@"children"] ;
+                
+                NSDictionary *child = nil ;
+                //10%的返回事件
+                if(seed < 3){
+                    if([children count] > 0){
+                        
+                        child = [children lastObject] ;
+                        NSLog(@"haleli >>>> test monkey,back action , click name : %@ ",[child objectForKey:@"name"]) ;
+                        if(![[[child objectForKey:@"name"] lowercaseString] containsString:@"back"]){
+                            [self touchesWithPoint:CGPointMake(344,32)];
+                            return ;
+                        }
+                        
+                    }
+                    //70%的概率发送UI事件
+                }else{
+                    if([children count] > 1){
+                        int random = arc4random() % ([children count] - 1);
+                        child = [children objectAtIndex:random] ;
+                        
+                        NSLog(@"haleli >>>> test monkey,ui action , click name : %@ ",[child objectForKey:@"name"]) ;
+                    }
+                }
+                
+                NSDictionary *payload = [child objectForKey:@"payload"] ;
+                
+                NSArray *pos = [payload objectForKey:@"pos"] ;
+                
+                
+                double x = [[pos objectAtIndex:0] doubleValue] * width;
+                double y = [[pos objectAtIndex:1] doubleValue] * height;
+                [self touchesWithPoint:CGPointMake(x,y)];
+                
+                
+            }else{
+                NSLog(@"cocos creator don't get tree") ;
+            }
         }
+        //ios 页面
+    }else{
+        NSLog(@"haleli >>>> 页面 属于 iOS") ;
+        [self randomIOSMonkey] ;
     }
 }
 
